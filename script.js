1ea10296c6fc61c51eb05ac611234834a1e0596f
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-04-13 14:25:10 +0530

    func() added to find next palindrome date & doc..

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const getAllDateFormatsStringList = (dateObj) => {
     month: "",
     year: "",
   };
+
+//   manage number less then day and month in date.
   if (dateObj.day < 10) newDataObj.day = "0" + dateObj.day;
   else newDataObj.day = "" + dateObj.day;
 
@@ -17,6 +19,7 @@ const getAllDateFormatsStringList = (dateObj) => {
 
   newDataObj.year = "" + dateObj.year;
 
+  //create different formats of dates.
   let ddmmyyyy = newDataObj.day + newDataObj.month + newDataObj.year;
   let mmddyyyy = newDataObj.month + newDataObj.day + newDataObj.year;
   let yyyymmdd = newDataObj.year + newDataObj.month + newDataObj.day;
@@ -27,19 +30,63 @@ const getAllDateFormatsStringList = (dateObj) => {
   return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
 };
 
-
+// method to check if date is palindrome in any formate
 const isDatePalindrome = (dateObj) => {
-const dateInAllFormats = getAllDateFormatsStringList(dateObj) ;
+  const dateInAllFormats = getAllDateFormatsStringList(dateObj);
   for (i = 0; i < dateInAllFormats.length; i++) {
     if (isPalindrome(dateInAllFormats[i])) return true;
   }
   return false;
 };
 
+// method to check leap year
+const isLeapYear = (year) =>
+  (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+// method to increment date by one day
+const incrementDate = (date) => {
+  let day = date.day + 1;
+  let month = date.month;
+  let year = date.year;
+
+  const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  // check if month  is fab and year is leap
+  if (month == 2 && isLeapYear(year)) {
+    if (day > 29) {
+      day = 1;
+      month++;
+    }
+  } else if (day > daysInMonth[month - 1]) {
+    day = 1;
+    month++;
+  }
+  if (month > 12) {
+    month = 1;
+    year++;
+  }
+
+  return {
+    day: day,
+    month: month,
+    year: year,
+  };
+};
+
+//method that find next palindrome day
+const getNextPalindromeDate = (dateObj) => {
+  let counter = 0;
+  while (1) {
+    counter++;
+    dateObj = incrementDate(dateObj);
+    if (isDatePalindrome(dateObj)) return [counter, dateObj];
+  }
+};
+
 dateObj = {
-  day: 02,
-  month: 02,
+  day: 23,
+  month: 1,
   year: 2020,
 };
 
-console.log(isDatePalindrome(dateObj));
+console.log(getNextPalindromeDate(dateObj));
